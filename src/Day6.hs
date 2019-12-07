2158463b4040efc687b62c8a873031a8d79deb8d
Benjamin Weber <mail@bwe.im>
2019-12-07 20:15:48 +0100

    Day 6.1: simplify data structure Orbit by incorporating Around and Planet directly (hence removing NoOrbit)

--- src/Day6.hs
+++ src/Day6.hs
@@ -6,9 +6,7 @@ import Data.Maybe (mapMaybe)
 
 import qualified Data.ByteString.Char8 as BS8 (ByteString, drop, length, lines, pack, readFile, takeWhile)
 
-data Around = Planet BS8.ByteString | UniversalCenterOfMass deriving (Show)
-
-data Relation = Orbit Relation Relation | NoOrbit Around deriving (Show)
+data Relation = Orbit Relation Relation | Planet BS8.ByteString | UniversalCenterOfMass deriving (Show, Eq)
 
 type Count = Int
 
@@ -18,19 +16,32 @@ getInput = (mapMaybe parseMap . BS8.lines <$>) . BS8.readFile
 -- |
 --
 -- >>> parseMap $ BS8.pack "COM)B"
--- Just (Orbit (NoOrbit UniversalCenterOfMass) (NoOrbit (Planet "B")))
+-- Just (Orbit UniversalCenterOfMass (Planet "B"))
 --
 -- >>> parseMap $ BS8.pack "B)C"
--- Just (Orbit (NoOrbit (Planet "B")) (NoOrbit (Planet "C")))
+-- Just (Orbit (Planet "B") (Planet "C"))
 parseMap :: BS8.ByteString -> Maybe Relation
 parseMap xs
-  | first == BS8.pack "COM" = Just $ Orbit (NoOrbit UniversalCenterOfMass) (NoOrbit (Planet second))
-  | otherwise = Just $ Orbit (NoOrbit (Planet first)) (NoOrbit (Planet second))
+  | first == BS8.pack "COM" = Just $ Orbit UniversalCenterOfMass (Planet second)
+  | otherwise = Just $ Orbit (Planet first) (Planet second)
   where
     first = BS8.takeWhile (/= ')') xs
     second = BS8.drop (BS8.length first + 1) xs
 
--- :: Relation -> Mapping -> Maybe Relation
+-- | Merge two `Relation`s if one is part of the other.
+-- 
+-- >>> mergeRelations (Orbit UniversalCenterOfMass (Planet "B")) $ Orbit (Planet "B") (Planet "C")
+-- Just (Orbit UniversalCenterOfMass (Orbit (Planet "B") (Planet "C")))
+--
+-- >>> mergeRelations (Orbit UniversalCenterOfMass (Orbit (Planet "B") (Planet "C"))) $ Orbit (Planet "C") (Planet "D")
+-- Just (Orbit UniversalCenterOfMass (Orbit (Planet "B")) (Orbit (Planet "C") (Planet "D")))
+mergeRelations :: Relation -> Relation -> Maybe Relation
+mergeRelations (Orbit first second) (Orbit third fourth)
+  | first == third = Just $ Orbit (Orbit first (Orbit third fourth)) second
+  | second == third = Just $ Orbit first (Orbit second fourth)
+  | otherwise = Nothing
+
+-- Relation Relation   NoOrbit
 
 
 directOrbits :: [Relation] -> Count
