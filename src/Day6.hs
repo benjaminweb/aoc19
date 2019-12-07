ca14ef138b04a5dd3e3acb5689ff4d83fd5260cd
Benjamin Weber <mail@bwe.im>
2019-12-07 19:49:17 +0100

    Day 6.1: change parseMap to return Relation directly (instead of Mapping)

--- src/Day6.hs
+++ src/Day6.hs
@@ -4,14 +4,9 @@ module Day6 where
 
 import qualified Data.ByteString.Char8 as BS8 (ByteString, drop, length, lines, pack, readFile, takeWhile)
 
-type Planet = BS8.ByteString
-
--- data Orbit = WhatAroundWhat Planet Orbit | UniversalCenterOfMass Planet deriving Show
-newtype Orbit = Orbit ()
-
 data Around = Planet BS8.ByteString | UniversalCenterOfMass deriving (Show)
 
-data Mapping = Mapping (Around, Around) deriving (Show)
+data Relation = Orbit Relation Relation | NoOrbit Around deriving (Show)
 
 type Count = Int
 
@@ -21,23 +16,26 @@ getInput = (BS8.lines <$>) . BS8.readFile
 -- |
 --
 -- >>> parseMap $ BS8.pack "COM)B"
--- Just (Mapping (UniversalCenterOfMass,Planet "B"))
+-- Just (Orbit (NoOrbit UniversalCenterOfMass) (NoOrbit (Planet "B")))
 --
 -- >>> parseMap $ BS8.pack "B)C"
--- Just (Mapping (Planet "B",Planet "C"))
-parseMap :: BS8.ByteString -> Maybe Mapping
+-- Just (Orbit (NoOrbit (Planet "B")) (NoOrbit (Planet "C")))
+parseMap :: BS8.ByteString -> Maybe Relation
 parseMap xs
-  | first == BS8.pack "COM" = Just $ Mapping (UniversalCenterOfMass, Planet second)
-  | otherwise = Just $ Mapping (Planet first, Planet second)
+  | first == BS8.pack "COM" = Just $ Orbit (NoOrbit UniversalCenterOfMass) (NoOrbit (Planet second))
+  | otherwise = Just $ Orbit (NoOrbit (Planet first)) (NoOrbit (Planet second))
   where
     first = BS8.takeWhile (/= ')') xs
     second = BS8.drop (BS8.length first + 1) xs
 
-directOrbits :: [Orbit] -> Count
+-- :: Relation -> Mapping -> Maybe Relation
+
+
+directOrbits :: [Relation] -> Count
 directOrbits = undefined
 
-indirectOrbits :: [Orbit] -> Count
+indirectOrbits :: [Relation] -> Count
 indirectOrbits = undefined
 
-totalOrbits :: [Orbit] -> Count
+totalOrbits :: [Relation] -> Count
 totalOrbits xs = directOrbits xs + indirectOrbits xs
